8e4b8e218e15014fb710b9bada7f706a772ffd2f
Amit Paul <amitpaul1304@gmail.com>
2023-09-23 18:48:41 +0600

    issue #65 Text was getting removed
    Changed method for pattern22.
    
    Signed-off-by: Amit Paul <amitpaul1304@gmail.com>

--- icici_bank.py
+++ icici_bank.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 import extracting_utility
 import camelot
 import re
+import matplotlib.pyplot as plt
 
 Success = False
 
@@ -19,6 +20,61 @@ def initialize(pdf_file, csv_output):
             break
 
 
+# # Done
+# # 7_ICICI_detailStatement_19-5-2021@11-44-35.pdf
+# # FASHION.FORWARD.01.01.2023.TO.28.02.2023
+# # pattern: "Sr No Value Date Transactio\nn DateCheque\nNumberTransactio\nn RemarksDebit\nAmountCredit\nAmountBalance(IN\nR)"
+# def Pattern22(pdf_file, csv_output):
+#     pattern_text = "Sr No Value Date Transactio\nn DateCheque\nNumberTransactio\nn RemarksDebit\nAmountCredit\nAmountBalance(IN\nR)"
+#     if not extracting_utility.search_keyword_in_pdf(pdf_file, pattern_text):
+#         return
+
+#     Bank_Name = "ICICI Bank"
+#     extracting_utility.print_info(
+#         inspect.currentframe().f_code.co_name, Bank_Name, extracting_utility.Page_Num
+#     )
+
+#     date_pattern = r"\d{2}-[A-Za-z]{3}-\n\d{4}"
+#     tables = camelot.read_pdf(pdf_file, flavor="lattice", pages="all")
+#     df_total = pd.DataFrame()
+#     for i in tqdm(range(tables.n)):
+#         df = tables[i].df
+#         # Remove trailing backslashes from all cells
+#         df = df.applymap(lambda x: x.rstrip("\/"))
+#         j = 0
+#         merged_row = []
+#         if i == 0:
+#             merged_row = [
+#                 [
+#                     "Sr No",
+#                     "Value Date",
+#                     "Transaction Date",
+#                     "Cheque Number",
+#                     "Transaction Remarks",
+#                     "Debit Amount",
+#                     "Credit Amount",
+#                     "Balance(INR)",
+#                 ]
+#             ]
+
+#         while j < (len(df)):
+#             date_match = re.search(date_pattern, df.loc[j, 1])
+#             if date_match:
+#                 if "EP\n" in df.loc[j, 4]:
+#                     df.loc[j, 4] = df.loc[j, 4].replace("EP\n", "")
+#                 merged_row.append(df.loc[j])
+#             j += 1
+#         df = pd.DataFrame(merged_row)
+#         df_total = pd.concat([df_total, df], axis=0).reset_index(drop=True)
+
+#     df = df_total.drop_duplicates().reset_index(drop=True)
+#     df.to_csv(csv_output, mode="a", index=False, header=False)
+
+#     global Success
+#     Success = True
+#     return
+
+
 # Done
 # 7_ICICI_detailStatement_19-5-2021@11-44-35.pdf
 # FASHION.FORWARD.01.01.2023.TO.28.02.2023
@@ -32,40 +88,54 @@ def Pattern22(pdf_file, csv_output):
     extracting_utility.print_info(
         inspect.currentframe().f_code.co_name, Bank_Name, extracting_utility.Page_Num
     )
-
+    cols = ["85,156,230,288,361,418,483"]
+    cols *= 128
     date_pattern = r"\d{2}-[A-Za-z]{3}-\n\d{4}"
-    tables = camelot.read_pdf(pdf_file, flavor="lattice", pages="all")
+    tables = camelot.read_pdf(
+        pdf_file, flavor="stream", pages="all", row_tol=15, columns=cols
+    )
     df_total = pd.DataFrame()
     for i in tqdm(range(tables.n)):
         df = tables[i].df
-        # Remove trailing backslashes from all cells
+        # camelot.plot(tables[i], kind='grid')
+        # plt.show(block=True)
         df = df.applymap(lambda x: x.rstrip("\/"))
-        j = 0
-        merged_row = []
-        if i == 0:
-            merged_row = [
-                [
-                    "Sr No",
-                    "Value Date",
-                    "Transaction Date",
-                    "Cheque Number",
-                    "Transaction Remarks",
-                    "Debit Amount",
-                    "Credit Amount",
-                    "Balance(INR)",
-                ]
-            ]
-
-        while j < (len(df)):
-            date_match = re.search(date_pattern, df.loc[j, 1])
-            if date_match:
-                if "EP\n" in df.loc[j, 4]:
-                    df.loc[j, 4] = df.loc[j, 4].replace("EP\n", "")
-                merged_row.append(df.loc[j])
-            j += 1
-        df = pd.DataFrame(merged_row)
         df_total = pd.concat([df_total, df], axis=0).reset_index(drop=True)
 
+    j = 0
+
+    merged_row = [
+        [
+            "Sr No",
+            "Value Date",
+            "Transaction Date",
+            "Cheque Number",
+            "Transaction Remarks",
+            "Debit Amount",
+            "Credit Amount",
+            "Balance(INR)",
+        ]
+    ]
+
+    while j < (len(df_total)):
+        date_match = re.search(date_pattern, df_total.loc[j, 1])
+        if date_match:
+            if "EP\n" in df_total.loc[j, 4]:
+                df_total.loc[j, 4] = df_total.loc[j, 4].replace("EP\n", "")
+            k = j + 1
+            new_row = df_total.loc[j]
+            if k < (len(df_total)):
+                next_date_match = re.search(date_pattern, df_total.loc[k, 1])
+            while k < (len(df_total)) and not next_date_match:
+                new_row += "\n" + df_total.loc[k]
+                j += 1
+                k += 1
+                if k < (len(df_total)):
+                    next_date_match = re.search(date_pattern, df_total.loc[k, 1])
+            merged_row.append(new_row)
+        j += 1
+    df = pd.DataFrame(merged_row)
+    df = df.applymap(extracting_utility.remove_trailing_newline)
     df = df_total.drop_duplicates().reset_index(drop=True)
     df.to_csv(csv_output, mode="a", index=False, header=False)
 
