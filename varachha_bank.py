5c1b6160f2cdddfd103f72a9278166ccd4bcb493
Amit Paul <amitpaul1304@gmail.com>
2023-11-07 01:31:28 +0600

    issue #39 #83
    varacha bank issue
    
    Signed-off-by: Amit Paul <amitpaul1304@gmail.com>

--- varachha_bank.py
+++ varachha_bank.py
@@ -35,14 +35,25 @@ def Pattern18(pdf_file, csv_output):
         inspect.currentframe().f_code.co_name, Bank_Name, extracting_utility.Page_Num
     )
     # print(csv_output)
-    cols = ["179,298,579,728,857,990,1130"]
+    cols = ["90,148,300,368,435,500"]
     cols *= 128
-    tables = camelot.read_pdf(pdf_file, flavor="lattice", pages="all")
+    TA = ["0,550,574,0"]
+    TA *= 128
+    tables_first_page = camelot.read_pdf(
+        pdf_file,
+        flavor="stream",
+        pages="1",
+        row_tol=20,
+        # edge_tol = 150,
+        split_text=True,
+        columns=cols,
+        table_areas=TA,
+    )
+    # tables_first_page.export('foo.csv', f='csv')
     df_total = pd.DataFrame()
-    for i in tqdm(range(tables.n)):
-        df = tables[i].df
-        # camelot.plot(tables[i], kind='textedge')
-        # plt.show(block=True)
+    for i in tqdm(range(tables_first_page.n)):
+        df = tables_first_page[i].df
+        # extracting_utility.show_plot_graph(tables_first_page[i])
         date_pattern = r"\d{2}-\d{2}-\d{4}"
         pattern = r"\|"
         j = 0
@@ -59,6 +70,58 @@ def Pattern18(pdf_file, csv_output):
                     "Closing  Bal",
                 ]
             ]
+
+        while j < (len(df)):
+            match_ = re.search(pattern, df.loc[j, 0])
+            date_match = re.search(date_pattern, df.loc[j, 0])
+            date_match_2 = re.search(date_pattern, df.loc[j, 1])
+
+            if date_match:
+                if match_:
+                    str1 = df.loc[j, 0][: match_.start()]
+                    str2 = df.loc[j, 0][match_.start() + 1 :]
+                    df.loc[j, 0] = str1
+                    df.loc[j, 1] = str2
+                elif len(df.loc[j, 1]) > 10:
+                    str1 = df.loc[j, 1][:10]
+                    str2 = df.loc[j, 1][10:] + df.loc[j, 2]
+                    df.loc[j, 1] = str1
+                    df.loc[j, 2] = str2
+                elif len(df.loc[j, 1]) < 1:
+                    str1 = df.loc[j, 2][:10]
+                    str2 = df.loc[j, 2][10:]
+                    df.loc[j, 1] = str1
+                    df.loc[j, 2] = str2
+                if "OPENING" in df.loc[j, 1] or "CLOSING" in df.loc[j, 1]:
+                    df.loc[j, 2] = df.loc[j, 1] + df.loc[j, 2]
+                    df.loc[j, 1] = ""
+
+                merged_row.append(df.loc[j])
+            j += 1
+        df = pd.DataFrame(merged_row)
+        df_total = pd.concat([df_total, df], axis=0).reset_index(drop=True)
+    print(df_total)
+
+    tables = camelot.read_pdf(pdf_file, flavor="lattice", pages=f"2-{extracting_utility.Page_Num}")
+    for i in tqdm(range(tables.n)):
+        df = tables[i].df
+        # extracting_utility.show_plot_graph(tables[i])
+        date_pattern = r"\d{2}-\d{2}-\d{4}"
+        pattern = r"\|"
+        j = 0
+        merged_row = []
+        # if i == 0:
+        #     merged_row = [
+        #         [
+        #             "TRN.  Date",
+        #             "Value  Date",
+        #             "Narration",
+        #             "Chq/Ref.No",
+        #             "Debit",
+        #             "Credit",
+        #             "Closing  Bal",
+        #         ]
+        #     ]
 
         while j < (len(df)):
             match_ = re.search(pattern, df.loc[j, 0])
