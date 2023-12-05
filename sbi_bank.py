05a3c55d1b436e47c083bfcfbc3174ab285e2d76
Amit Paul <amitpaul1304@gmail.com>
2023-12-05 10:23:19 +0600

    Added new pattern for SBI bank
    Signed-off-by: Amit Paul <amitpaul1304@gmail.com>

--- sbi_bank.py
+++ sbi_bank.py
@@ -11,6 +11,10 @@ Success = False
 def initialize(pdf_file, csv_output):
     patterns = [
         Pattern4,
+        PatternSBI2,
+        PatternSBI3,
+        PatternSBI4,
+        PatternSBI5,
     ]
     for pattern in patterns:
         pattern(pdf_file, csv_output)
@@ -55,8 +59,8 @@ def Pattern4(pdf_file, csv_output):
                     "Txn Date",
                     "Value Date",
                     "Description",
-                    "Ref No./Cheque",
-                    "No.Branch Code",
+                    "Ref No./Cheque No.",
+                    "Branch Code",
                     "Debit",
                     "Credit",
                     "Balance",
@@ -113,4 +117,294 @@ def Pattern4(pdf_file, csv_output):
             df_total = df_total.drop_duplicates().reset_index(drop=True)
         df_total.to_csv(csv_output, mode="a", index=False, header=False)
         Success = True
+    return
+
+# Done
+# Account_Statement_from_1_Oct_2023_to_31_Oct_2023_1701060585.pdf
+# pattern: "Txn Date Value Date Description Ref No./Cheque\nNo.Branch\nCodeDebit Credit Balance"
+def PatternSBI2(pdf_file, csv_output):
+    pattern_text = "Txn Date Value Date Description Ref No./Cheque\nNo.Branch\nCodeDebit Credit Balance"
+    if not extracting_utility.search_keyword_in_pdf(pdf_file, pattern_text):
+        return
+
+    Bank_Name = "State Bank of India (SBI)"
+    extracting_utility.print_info(
+        inspect.currentframe().f_code.co_name, Bank_Name, extracting_utility.Page_Num
+    )
+    # cols = [""]
+    # cols *= 128
+    # TA = [""]
+    # TA *= 128
+    # tables = camelot.read_pdf(
+    #     pdf_file, flavor="stream", pages="all", columns=cols, table_areas=TA
+    # )
+    tables = camelot.read_pdf(
+        pdf_file, flavor="lattice", pages="all",
+    )
+    df_total = pd.DataFrame()
+    for i in tqdm(range(tables.n)):
+        df = tables[i].df
+        # extracting_utility.show_plot_graph(tables[i])
+        df_total = pd.concat([df_total, df], axis=0).reset_index(drop=True)
+    df = df_total
+    date_pattern = r"\d{2}/\d{2}/\d{4}"
+    merged_row = [
+        [
+            "Txn Date",
+            "Value Date",
+            "Description",
+            "Ref No./Cheque No.",
+            "Branch Code",
+            "Debit",
+            "Credit",
+            "Balance",
+        ]
+    ]
+
+    j = 0
+    while j < (len(df)):
+        date_match = re.search(date_pattern, df.loc[j, 0])
+        if date_match:
+            value_date_match = re.search(date_pattern, df.loc[j, 1])
+            if not value_date_match:
+                split_row = df.loc[j,2].split(maxsplit=1)
+                df.loc[j, 1] = split_row[0] 
+                df.loc[j, 2] = split_row[1]
+                print(df.loc[j])
+            k = j + 1
+            new_row = df.loc[j]
+            while k < (len(df)):
+                next_date_match = re.search(date_pattern, df.loc[k, 0])
+                
+                if (
+                    next_date_match
+                    or df.loc[k, 0] != ""
+                    or df.loc[k, 2] != ""
+                    or df.loc[k, 3] != ""
+                ):
+                    break
+                new_row += "\n" + df.loc[k]
+                j += 1
+                k += 1
+            merged_row.append(new_row)
+        j += 1
+    df = pd.DataFrame(merged_row)
+    df.to_csv(csv_output, mode="w", index=False, header=False)
+    global Success
+    Success = True
+    return
+
+# Done
+# NIRMYA_10. JANUARY 2023.pdf
+# pattern: "Date Transaction Reference Ref.No./Chq.No. Credit Debit Balance"
+def PatternSBI3(pdf_file, csv_output):
+    pattern_text = "Date Transaction Reference Ref.No./Chq.No. Credit Debit Balance"
+    if not extracting_utility.search_keyword_in_pdf(pdf_file, pattern_text):
+        return
+
+    Bank_Name = "State Bank of India (SBI)"
+    extracting_utility.print_info(
+        inspect.currentframe().f_code.co_name, Bank_Name, extracting_utility.Page_Num
+    )
+    # cols = [""]
+    # cols *= 128
+    # TA = [""]
+    # TA *= 128
+    # tables = camelot.read_pdf(
+    #     pdf_file, flavor="stream", pages="all",
+    #     # columns=cols, table_areas=TA
+    # )
+    tables = camelot.read_pdf(
+        pdf_file, flavor="lattice", pages="all",
+        line_scale = 80
+    )
+    df_total = pd.DataFrame()
+    for i in tqdm(range(tables.n)):
+        df = tables[i].df
+        # extracting_utility.show_plot_graph(tables[i])
+        df_total = pd.concat([df_total, df], axis=0).reset_index(drop=True)
+    df = df_total
+    # df.to_csv("csv_output.csv", mode="w", index=False, header=False)
+    
+    date_pattern = r"\d{2}-\d{2}-\d{2}"
+    merged_row = [
+        [
+            "Date",
+            "Transaction Reference",
+            "Ref.No./Chq.No.",
+            "Credit",
+            "Debit",
+            "Balance",
+        ]
+    ]
+
+    j = 0
+    while j < (len(df)):
+        date_match = re.search(date_pattern, df.loc[j, 0])
+        if date_match and len(df.loc[j])>5 and df.loc[j, 5]!= "":
+            k = j + 1
+            new_row = df.loc[j]
+            while k < (len(df)):
+                next_date_match = re.search(date_pattern, df.loc[k, 0])
+                
+                if (
+                    next_date_match
+                    or df.loc[k, 0] != ""
+                    or df.loc[k, 2] != ""
+                    or df.loc[k, 3] != ""
+                ):
+                    break
+                new_row += "\n" + df.loc[k]
+                j += 1
+                k += 1
+            merged_row.append(new_row)
+        j += 1
+    df = pd.DataFrame(merged_row)
+    df.to_csv(csv_output, mode="w", index=False, header=False)
+    global Success
+    Success = True
+    return
+
+# Done
+# NISHKAM_15.04.2022 TO 05.04.2023.pdf
+# pattern: "Txn Date Value Date DescriptionRef/Cheque\nNo.Debit Credit Balance"
+def PatternSBI4(pdf_file, csv_output):
+    pattern_text = "Txn Date Value Date DescriptionRef/Cheque\nNo.Debit Credit Balance"
+    if not extracting_utility.search_keyword_in_pdf(pdf_file, pattern_text):
+        return
+
+    Bank_Name = "State Bank of India (SBI)"
+    extracting_utility.print_info(
+        inspect.currentframe().f_code.co_name, Bank_Name, extracting_utility.Page_Num
+    )
+    # cols = [""]
+    # cols *= 128
+    # TA = [""]
+    # TA *= 128
+    # tables = camelot.read_pdf(
+    #     pdf_file, flavor="stream", pages="all",
+    #     # columns=cols, table_areas=TA
+    # )
+    tables = camelot.read_pdf(
+        pdf_file, flavor="lattice", pages="all",
+        # line_scale = 80
+    )
+    df_total = pd.DataFrame()
+    for i in tqdm(range(tables.n)):
+        df = tables[i].df
+        # extracting_utility.show_plot_graph(tables[i])
+        df_total = pd.concat([df_total, df], axis=0).reset_index(drop=True)
+    df = df_total
+    # df.to_csv("csv_output.csv", mode="w", index=False, header=False)
+    
+    date_pattern = r"\d{2}-\d{2}-\d{2}"
+    merged_row = [
+        [
+            "Txn Date",
+            "Value Date",
+            "Description",
+            "Ref/Cheque No.",
+            "Debit",
+            "Credit",
+            "Balance",
+        ]
+    ]
+
+    j = 0
+    while j < (len(df)):
+        date_match = re.search(date_pattern, df.loc[j, 0])
+        if date_match and len(df.loc[j])>6 and df.loc[j, 6]!= "":
+            k = j + 1
+            new_row = df.loc[j]
+            while k < (len(df)):
+                next_date_match = re.search(date_pattern, df.loc[k, 0])
+                
+                if (
+                    next_date_match
+                    or df.loc[k, 0] != ""
+                    or df.loc[k, 2] != ""
+                    or df.loc[k, 3] != ""
+                ):
+                    break
+                new_row += "\n" + df.loc[k]
+                j += 1
+                k += 1
+            merged_row.append(new_row)
+        j += 1
+    df = pd.DataFrame(merged_row)
+    df.to_csv(csv_output, mode="w", index=False, header=False)
+    global Success
+    Success = True
+    return
+
+# Done
+# 
+# pattern: "Date Credit Balance DetailsRef No./Cheque"
+def PatternSBI5(pdf_file, csv_output):
+    pattern_text = "Date Credit Balance DetailsRef No./Cheque"
+    if not extracting_utility.search_keyword_in_pdf(pdf_file, pattern_text):
+        return
+
+    Bank_Name = "State Bank of India (SBI)"
+    extracting_utility.print_info(
+        inspect.currentframe().f_code.co_name, Bank_Name, extracting_utility.Page_Num
+    )
+    # cols = [""]
+    # cols *= 128
+    # TA = [""]
+    # TA *= 128
+    # tables = camelot.read_pdf(
+    #     pdf_file, flavor="stream", pages="all",
+    #     # columns=cols, table_areas=TA
+    # )
+    tables = camelot.read_pdf(
+        pdf_file, flavor="lattice", pages="all",
+        line_scale = 80
+    )
+    df_total = pd.DataFrame()
+    print(tables.n)
+    for i in tqdm(range(tables.n)):
+        df = tables[i].df
+        # extracting_utility.show_plot_graph(tables[i])
+        df_total = pd.concat([df_total, df], axis=0).reset_index(drop=True)
+    df = df_total
+    # df.to_csv("csv_output.csv", mode="w", index=False, header=False)
+    
+    date_pattern = r"\d{2} [A-Za-z]{3} \d{4}"
+    merged_row = [
+        [
+            "Date",
+            "Details",
+            "Ref No./Cheque No",
+            "Debit",
+            "Credit",
+            "Balance",
+        ]
+    ]
+
+    j = 0
+    while j < (len(df)):
+        date_match = re.search(date_pattern, df.loc[j, 0])
+        if date_match and len(df.loc[j])>5 and df.loc[j, 5]!= "":
+            k = j + 1
+            new_row = df.loc[j]
+            while k < (len(df)):
+                next_date_match = re.search(date_pattern, df.loc[k, 0])
+                
+                if (
+                    next_date_match
+                    or df.loc[k, 0] != ""
+                    or df.loc[k, 2] != ""
+                    or df.loc[k, 3] != ""
+                ):
+                    break
+                new_row += "\n" + df.loc[k]
+                j += 1
+                k += 1
+            merged_row.append(new_row)
+        j += 1
+    df = pd.DataFrame(merged_row)
+    df.to_csv(csv_output, mode="w", index=False, header=False)
+    global Success
+    Success = True
     return
