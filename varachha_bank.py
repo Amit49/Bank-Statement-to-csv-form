88d3b77d7514c012e65e3814e902d1209608d379
Amit Paul <amitpaul1304@gmail.com>
2023-12-12 21:54:28 +0600

    New patterns for VARCHHA bank
    Signed-off-by: Amit Paul <amitpaul1304@gmail.com>

--- varachha_bank.py
+++ varachha_bank.py
@@ -11,6 +11,9 @@ Success = False
 def initialize(pdf_file, csv_output):
     patterns = [
         Pattern18,
+        PatternVarachha2,
+        PatternVarachha3,
+        PatternVarachha4,
         Default,
     ]
     for pattern in patterns:
@@ -173,4 +176,128 @@ def Default(pdf_file, csv_output):
         # Remove trailing backslashes from all cells
         df = df.applymap(lambda x: x.rstrip("\/"))
         df.to_csv(csv_output, mode="a", index=False, header=False)
+    return
+
+# Done
+# Detail03-Dec-2023-06-27-10-PM_1701674694_decrypt.pdf
+# pattern: "DateValue DateNarrationChq/Ref. NoDebitCreditClosing Bal"
+def PatternVarachha2(pdf_file, csv_output):
+    pattern_text = "DateValue DateNarrationChq/Ref. NoDebitCreditClosing Bal"
+    if not extracting_utility.search_keyword_in_pdf(pdf_file, pattern_text):
+        return
+
+    Bank_Name = "VARCHHA BANK"
+    extracting_utility.print_info(
+        inspect.currentframe().f_code.co_name, Bank_Name, extracting_utility.Page_Num
+    )
+    cols = [""]
+    cols *= 128
+    TA = [""]
+    TA *= 128
+    tables = camelot.read_pdf(
+        pdf_file, flavor="lattice", pages="all"
+    )
+    df_total = pd.DataFrame()
+    for i in tqdm(range(tables.n)):
+        df = tables[i].df
+        # extracting_utility.show_plot_graph(tables[i])
+        df = extracting_utility.filter_dataframe(df,0,"Date",1)
+        df_total = pd.concat([df_total, df], axis=0).reset_index(drop=True)
+    df = df_total
+    print(df.to_markdown(tablefmt="grid"))
+    df.to_csv(csv_output, mode="w", index=False, header=False)
+    global Success
+    Success = True
+    return
+
+# Done
+# Detail03-Dec-2023-06-27-10-PM_1701674694_decrypt.pdf
+# pattern: "Date Narration Cheque\nNo./Ref.NoWithdrawal Deposit Balance"
+def PatternVarachha3(pdf_file, csv_output):
+    pattern_text1 = "Date Narration Cheque\nNo./Ref.NoWithdrawal Deposit Balance"
+    pattern_text2 = "Date Narration Cheque No./Ref.No Withdrawal Deposit Balance"
+    if(
+        not extracting_utility.search_keyword_in_pdf(pdf_file, pattern_text1) and 
+        not extracting_utility.search_keyword_in_pdf(pdf_file, pattern_text2)
+    ):
+        return
+
+    Bank_Name = "VARCHHA BANK"
+    extracting_utility.print_info(
+        inspect.currentframe().f_code.co_name, Bank_Name, extracting_utility.Page_Num
+    )
+    tables = camelot.read_pdf(
+        pdf_file, flavor="lattice", pages="all"
+    )
+    df_total = pd.DataFrame()
+    for i in tqdm(range(tables.n)):
+        df = tables[i].df
+        # extracting_utility.show_plot_graph(tables[i])
+        df_total = pd.concat([df_total, df], axis=0).reset_index(drop=True)
+    df = df_total
+    df = extracting_utility.filter_dataframe(df,1,"Narration",1,True)
+    # print(df.to_markdown(tablefmt="grid"))
+    df.to_csv(csv_output, mode="w", index=False, header=False)
+    global Success
+    Success = True
+    return
+
+# Done
+# nov_23_cc_1701409030.pdf
+# pattern: "Date Narration Cheque/Ref No Debit Credit Closing Bal"
+def PatternVarachha4(pdf_file, csv_output):
+    pattern_text = "Date Narration Cheque/Ref No Debit Credit Closing Bal"
+    if(
+        not extracting_utility.search_keyword_in_pdf(pdf_file, pattern_text)
+    ):
+        return
+
+    Bank_Name = "VARCHHA BANK"
+    extracting_utility.print_info(
+        inspect.currentframe().f_code.co_name, Bank_Name, extracting_utility.Page_Num
+    )
+    tables = camelot.read_pdf(
+        pdf_file, flavor="lattice", pages="all"
+    )
+    df_total = pd.DataFrame()
+    for i in tqdm(range(tables.n)):
+        df = tables[i].df
+        # extracting_utility.show_plot_graph(tables[i])
+        df_total = pd.concat([df_total, df], axis=0).reset_index(drop=True)
+    df = df_total
+    
+    date_pattern = r"\d{2}-\d{2}-\d{4}"
+    
+    merged_row = [
+        [
+            "Date",
+            "Narration",
+            "Cheque/Ref No",
+            "Debit",
+            "Credit",
+            "Closing Bal",
+        ]
+    ]
+    
+    j = 0
+    while j < (len(df)):
+        date_match = re.search(date_pattern, df.loc[j, 0])
+        if date_match:
+            k = j + 1
+            new_row = df.loc[j]
+            while k < (len(df)):
+                next_date_match = re.search(date_pattern, df.loc[k, 0])
+                if (
+                    next_date_match
+                ):
+                    break
+                new_row += "\n" + df.loc[k]
+                j += 1
+                k += 1
+            merged_row.append(new_row)
+        j += 1
+    df = pd.DataFrame(merged_row)
+    df.to_csv(csv_output, mode="w", index=False, header=False)
+    global Success
+    Success = True
     return
