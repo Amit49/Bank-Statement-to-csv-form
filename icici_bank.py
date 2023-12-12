6dc56ef010d10d2f0ef7d66fd67d7406e6c1c17a
Amit Paul <amitpaul1304@gmail.com>
2023-12-12 21:53:16 +0600

    New Patterns for icici bank
    Signed-off-by: Amit Paul <amitpaul1304@gmail.com>

--- icici_bank.py
+++ icici_bank.py
@@ -17,6 +17,9 @@ def initialize(pdf_file, csv_output):
         PatternICICI4,
         PatternICICI5,
         PatternICICI6,
+        PatternICICI7,
+        PatternICICI8,
+        PatternICICI9,
         Default,
     ]
     for pattern in patterns:
@@ -365,6 +368,245 @@ def PatternICICI6(pdf_file, csv_output):
         j += 1
     df = pd.DataFrame(merged_row)
     df.to_csv(csv_output, mode="a", index=False, header=False)
+    global Success
+    Success = True
+    return
+
+# Done
+# ConsolidatedStatementReport_Dec2023_1701766571.pdf
+# pattern: "SR. NO. DATE TRANSACTION\nIDVALUE DATE PARTICULARS DEPOSITS WITHDRAWLS BALANCE"
+def PatternICICI7(pdf_file, csv_output):
+    pattern_text = "SR. NO. DATE TRANSACTION\nIDVALUE DATE PARTICULARS DEPOSITS WITHDRAWLS BALANCE"
+    if not extracting_utility.search_keyword_in_pdf(pdf_file, pattern_text):
+        return
+
+    Bank_Name = "ICICI Bank"
+    extracting_utility.print_info(
+        inspect.currentframe().f_code.co_name, Bank_Name, extracting_utility.Page_Num
+    )
+    cols = [""]
+    cols *= 128
+    TA = [""]
+    TA *= 128
+    # tables = camelot.read_pdf(
+    #     pdf_file, flavor="stream", pages="all", columns=cols, table_areas=TA
+    # )
+    tables = camelot.read_pdf(pdf_file, flavor="lattice", pages="all")
+    df_total = pd.DataFrame()
+    for i in tqdm(range(tables.n)):
+        df = tables[i].df
+        # extracting_utility.show_plot_graph(tables[i])
+        df_total = pd.concat([df_total, df], axis=0).reset_index(drop=True)
+    df = df_total
+    df.to_csv(csv_output, mode="w", index=False, header=False)
+    global Success
+    Success = True
+    return
+
+# Done
+# icici_bank_1701412856.pdf
+# pattern: "S No. Value Date Transaction Date Cheque Number Transaction Remarks Withdrawal Amount\n(INR )Deposit Amount\n(INR )Balance (INR )"
+def PatternICICI8(pdf_file, csv_output):
+    pattern_text = "S No. Value Date Transaction Date Cheque Number Transaction Remarks Withdrawal Amount\n(INR )Deposit Amount\n(INR )Balance (INR )"
+    if not extracting_utility.search_keyword_in_pdf(pdf_file, pattern_text):
+        return
+
+    Bank_Name = "ICICI Bank"
+    extracting_utility.print_info(
+        inspect.currentframe().f_code.co_name, Bank_Name, extracting_utility.Page_Num
+    )
+    cols = ["213,310,410,500,685,775,860"]
+    cols *= 128
+    TR = ["0,1340,950,0"]
+    TR *= 128
+
+    tables = camelot.read_pdf(
+        pdf_file, flavor="stream", pages="all", columns=cols, table_areas=TR, row_tol=12
+    )
+    # tables = camelot.read_pdf(pdf_file, flavor="lattice", pages="all")
+    # tables.export("foo.csv",f='csv')
+    df_total = pd.DataFrame()
+    for i in tqdm(range(tables.n)):
+        df = tables[i].df
+        # extracting_utility.show_plot_graph(tables[i])
+        df_total = pd.concat([df_total, df], axis=0).reset_index(drop=True)
+    df = df_total
+    date_pattern = r"\d{2}/\d{2}/\d{4}"
+
+    merged_row = [
+        [
+            "S No.",
+            "Value Date",
+            "Transaction Date",
+            "Cheque Number",
+            "Transaction Remarks",
+            "Withdrawal Amount (INR)",
+            "Deposit Amount (INR)",
+            "Balance(INR)",
+        ]
+    ]
+
+    j = 0
+    while j < (len(df)):
+        date_match = re.search(date_pattern, df.loc[j, 1])
+        if date_match:
+            k = j + 1
+            new_row = df.loc[j]
+            while k < (len(df)):
+                next_date_match = re.search(date_pattern, df.loc[k, 1])
+                if (
+                    next_date_match
+                    or df.loc[k, 2] != ""
+                    or df.loc[k, 3] != ""
+                ):
+                    break
+                new_row += "\n" + df.loc[k]
+                j += 1
+                k += 1
+            merged_row.append(new_row)
+        j += 1
+    df = pd.DataFrame(merged_row)
+    df.to_csv(csv_output, mode="w", index=False, header=False)
+    global Success
+    Success = True
+    return
+
+# TODO: need to make changes in
+#       C:\Users\username\AppData\Local\Programs\Python\Python311\Lib\site-packages\PyPDF2\_cmap.py
+'''
+def parse_bfrange(
+    l: bytes,
+    map_dict: Dict[Any, Any],
+    int_entry: List[int],
+    multiline_rg: Union[None, Tuple[int, int]],
+) -> Union[None, Tuple[int, int]]:
+    lst = [x for x in l.split(b" ") if x]
+    closure_found = False
+    if multiline_rg is not None:
+        fmt = b"%%0%dX" % (map_dict[-1] * 2)
+        a = multiline_rg[0]  # a, b not in the current line
+        b = multiline_rg[1]
+        for sq in lst[1:]:
+            if sq == b"]":
+                closure_found = True
+                break
+            map_dict[
+                unhexlify(fmt % a).decode(
+                    "charmap" if map_dict[-1] == 1 else "utf-16-be",
+                    "surrogatepass",
+                )
+            ] = unhexlify(sq).decode("utf-16-be", "surrogatepass")
+            int_entry.append(a)
+            a += 1
+    else:
+        a = int(lst[0], 16)
+        b = int(lst[1], 16)
+        nbi = max(len(lst[0]), len(lst[1]))
+        map_dict[-1] = ceil(nbi / 2)
+        fmt = b"%%0%dX" % (map_dict[-1] * 2)
+        if lst[2] == b"[":
+            for sq in lst[3:]:
+                if sq == b"]":
+                    closure_found = True
+                    break
+                map_dict[
+                    unhexlify(fmt % a).decode(
+                        "charmap" if map_dict[-1] == 1 else "utf-16-be",
+                        "surrogatepass",
+                    )
+                ] = unhexlify(sq).decode("utf-16-be", "surrogatepass")
+                int_entry.append(a)
+                a += 1
+        else:  # case without list
+            c = int(lst[2], 16)
+            fmt2 = b"%%0%dX" % max(4, len(lst[2]))
+            closure_found = True
+            while a <= b:
+                map_dict[
+                    unhexlify(fmt % a).decode(
+                        "charmap" if map_dict[-1] == 1 else "utf-16-be",
+                        "surrogatepass",
+                    )
+                ] = unhexlify(fmt2 % c).decode("utf-16-be", "surrogatepass")
+                int_entry.append(a)
+                a += 1
+                c += 1
+    return None if closure_found else (a, b)
+'''
+# DONE
+# Statement_2023MTH11_168559138_unlocked_1701768364 (1).pdf
+# pattern: "Tran D ateValu e Date Particulars Loca tion Chq.No Withdrawals Deposits Bala nce (INR)"
+def PatternICICI9(pdf_file, csv_output):
+    pattern_text = "Tran D ateValu e Date Particulars Loca tion Chq.No Withdrawals Deposits Bala nce (INR)"
+    if not extracting_utility.search_keyword_in_pdf(pdf_file, pattern_text):
+        return
+
+    Bank_Name = "ICICI Bank"
+    extracting_utility.print_info(
+        inspect.currentframe().f_code.co_name, Bank_Name, extracting_utility.Page_Num
+    )
+    cols = ["68,103,286,374,407,455,506"]
+    cols *= 128
+    TA = ["0,820,570,0"]
+    TA *= 128
+    tables = camelot.read_pdf(
+        pdf_file, flavor="stream", pages="all",
+        columns=cols, table_areas=TA,
+        split_text = True
+    )
+    df_total = pd.DataFrame()
+    for i in tqdm(range(tables.n)):
+        df = tables[i].df
+        # extracting_utility.show_plot_graph(tables[i])
+        df = extracting_utility.filter_dataframe(df,0,"Tran Date",1)
+        df = extracting_utility.filter_dataframe(df,2,"Total",2)
+        df_total = pd.concat([df_total, df], axis=0).reset_index(drop=True)
+    df = df_total
+    date_pattern = r"\d{2}-\d{2}-\d{4}"
+
+    merged_row = [
+        [
+            "Tran Date",
+            "Value Date",
+            "Particulars",
+            "Location",
+            "Chq.No",
+            "Withdrawals",
+            "Deposits",
+            "Balance (INR).",
+        ]
+    ]
+    for i in range(len(df)):
+        date_match = re.search(date_pattern, df.loc[i, 0])
+        if date_match:
+            if  df.loc[i, 2] == "":
+                if i > 0:
+                    df.loc[i, 2] = df.loc[i-1, 2]
+                    df.loc[i - 1, 2] = ""
+    j = 0
+    while j < (len(df)):
+        date_match = re.search(date_pattern, df.loc[j, 0])
+        if date_match:
+            k = j + 1
+            new_row = df.loc[j]
+            while k < (len(df)):
+                next_date_match = re.search(date_pattern, df.loc[k, 0])
+                if (
+                    next_date_match
+                    or df.loc[k, 1] != ""
+                    or df.loc[k, 3] != ""
+                    or df.loc[k, 4] != ""
+                ):
+                    break
+                new_row += "\n" + df.loc[k]
+                j += 1
+                k += 1
+            merged_row.append(new_row)
+        j += 1
+    df = pd.DataFrame(merged_row)
+    df = df.iloc[:, :8]
+    # print(df.to_markdown(tablefmt="grid"))
+    df.to_csv(csv_output, mode="w", index=False, header=False)
     global Success
     Success = True
     return
